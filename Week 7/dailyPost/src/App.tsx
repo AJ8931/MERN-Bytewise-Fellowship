9bf381f905c61526dfc4ba8e36feb15650b26536
Anas Jahangir <anasm8931@gmail.com>
2024-09-27 12:04:28 +0500

    Week 7 Project Initial setup

---
+++ Week 7/dailyPost/src/App.tsx
@@ -0,0 +1,15 @@
+import "./global.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold bg-blue-100 text-center py-8 leading-relaxed">
+        <span className="text-8xl">🌐</span>
+        <br />
+        Daily-Post
+      </h1>
+    </>
+  );
+}
+
+export default App;
